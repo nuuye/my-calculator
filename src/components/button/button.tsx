b10227d9b0875c0273d54bbec4a3539df5ec2606
nuuye <thomassmostowfi@gmail.com>
2022-07-11 23:47:27 +0200

    Finished main design, added some js and hooks

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -4,12 +4,16 @@ import "./button.scss";
 type Props = {
   character: string;
   whiteBackGround?: Boolean;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 };
-function Button({ character, whiteBackGround = false }: Props) {
+function Button({ character, whiteBackGround = false, onClick }: Props) {
   return (
-    <div className={`container ${whiteBackGround && "whiteBackGround"}`}>
+    <button
+      onClick={onClick}
+      className={`container ${whiteBackGround && "whiteBackGround"}`}
+    >
       <span>{character}</span>
-    </div>
+    </button>
   );
 }
 
--- src/components/calculator/calculator.tsx
+++ src/components/calculator/calculator.tsx
@@ -1,47 +1,100 @@
-import React, { useState } from "react";
-import "./calculator.scss";
-import Button from "../button/button";
+import React, { useEffect, useState } from 'react';
+import './calculator.scss';
+import Button from '../button/button';
+import {
+  atan2,
+  chain,
+  derivative,
+  e,
+  evaluate,
+  log,
+  pi,
+  pow,
+  round,
+  sqrt,
+} from 'mathjs';
 
 type Props = {};
 function Calculator({}: Props) {
   const [value, setValue] = useState<number>(0);
+  const [display, setDisplay] = useState<string[]>([]);
+
   const characterList: Array<string> = [
-    "%",
-    "CE",
-    "C",
-    "<-",
-    "1/x",
-    "x²",
-    "²√x",
-    "÷",
-    "7",
-    "8",
-    "9",
-    "x",
-    "4",
-    "5",
-    "6",
-    "-",
-    "1",
-    "2",
-    "3",
-    "+",
-    "+/-",
-    "0",
-    ",",
-    "=",
+    '%',
+    'CE',
+    'C',
+    '<-',
+    '1/x',
+    'x²',
+    '²√x',
+    '÷',
+    '7',
+    '8',
+    '9',
+    'x',
+    '4',
+    '5',
+    '6',
+    '-',
+    '1',
+    '2',
+    '3',
+    '+',
+    '+/-',
+    '0',
+    ',',
+    '=',
   ];
+
+  const addElementToDisplay = (element: string) => {
+    display && setDisplay([...display, element]);
+  };
+
+  const calculateDisplay = () => {
+    return evaluate(display.join(' '));
+  };
+
+  const handleClick = (event: any) => {
+    switch (event.target.innerText) {
+      case '<-':
+        display.pop();
+        break;
+      case 'CE':
+        setDisplay([]);
+        break;
+      case '=':
+        // setDisplay(calculateDisplay());
+        break;
+      default:
+        addElementToDisplay(event.target.innerText);
+        break;
+    }
+  };
+  const testArray = ['4', '*', '7'];
+  useEffect(() => {
+    console.log(display);
+    console.log('evaluate : ', evaluate(testArray.join(' ')));
+  }, [display]);
+
   return (
     <div className="mainContainer">
+      <div className="bander">
+        <span>Standard calculator</span>
+      </div>
       <header>
-        <div className="bander">
-          <span>Standard calculator</span>
+        <div className="contentContainer">
+          <span>{display.map((element) => element)}</span>
         </div>
-        <div className="contentContainer"></div>
       </header>
       <body>
-        {characterList.map((character) => (
-          <Button character={character} />
+        {characterList.map((character, key) => (
+          <Button
+            key={key}
+            character={character}
+            onClick={(e) => {
+              handleClick(e);
+            }}
+          />
         ))}
       </body>
     </div>
